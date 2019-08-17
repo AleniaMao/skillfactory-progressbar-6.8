68f0636ee3414d929f2472a58b91e21fb9d34aa0
AleniaMao <53580094+AleniaMao@users.noreply.github.com>
2019-08-17 18:19:04 +0300

    Add files via upload

---
+++ js/script-a6.js
@@ -0,0 +1,9 @@
+$('#button1').click(function() {
+   $('#progressbar').animate({value: '+=1'});
+});
+$('#button2').click(function() {
+   $('#progressbar').animate({value: '+=3'});
+});
+$('#button3').click(function() {
+   $('#progressbar').animate({value: '+=7'});
+});
